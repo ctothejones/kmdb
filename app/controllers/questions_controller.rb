b345cb340063d1eb09fae982ea946d7d06ce7782
Colin Jones <colinsjones11@gmail.com>
2014-05-03 15:09:40 -0500

    Q4 Done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -12,16 +12,21 @@ class QuestionsController < ApplicationController
   def question_3
     # How many movies does the director of the first movie have on the list?
 
-    @number_of_movies_directed_by_first_movie_director = Movie.where({ :director_id => Movie.first.director_id}).count
+    @number_of_movies_directed_by_first_movie_director = Movie.where({ :director_id => Movie.first.director_id }).count
   end
 
   def question_4
     # What is the most number of movies any single director has on the list?
 
-    # Your Ruby goes here.
-    # You'll probably have to use both ActiveRecord query methods as well as some plain old Ruby logic.
+    directors = []
 
-    # @most_number_of_movies_by_a_single_director = ???
+    Movie.all.each do |m|
+      d = Movie.where({ :director_id => m.director_id }).count
+      directors << d
+    end
+
+    @most_number_of_movies_by_a_single_director = directors.max
+
   end
 
   def question_5
